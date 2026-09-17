127a1ee389422e8ac2bfb81eb6c5ee308bdd3011
pedro-novaes00 <condyy456@gmail.com>
2026-09-17 20:32:18 -0300

    Update funcoes.js
    Signed-off-by: pedro-novaes00 <condyy456@gmail.com>

--- funcoes.js
+++ funcoes.js
@@ -1,18 +1,6 @@
 function calcularMedia(nota1, nota2) {
  return (nota1 + nota2) / 2;
 }
-function verificarSituacao(media) {
- if (media >= 6) {
- return "Aprovado";
- }
- return "Reprovado";
-}
-if (typeof module !== "undefined") {
- module.exports = {
- calcularMedia,
- verificarSituacao
- };
-}
 
 function verificarSituacao(media) {
  if (media >= 6) {
